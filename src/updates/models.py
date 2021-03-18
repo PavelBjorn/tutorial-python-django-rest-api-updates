ac718f679a525c0f196f1e5b4605d78eb0561c01
Pasha <pavel.fedor.develop@gmail.com>
2021-03-19 00:01:11 +0200

    Initial commit

---
+++ src/updates/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.conf import settings
+
+
+def upload_update_image(instance, filename):
+    return "updates/{user}/{filename}".format(user=instance.user, filename=filename)
+
+
+class Update(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, models.CASCADE)
+    content = models.TextField()
+    image = models.ImageField(upload_to=upload_update_image, blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content or ""
