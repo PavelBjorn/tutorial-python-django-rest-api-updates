255b41a50bbeb988d038876a96fc2d034f0b3cc2
PavelF <pavlof@wix.com>
2021-03-20 13:44:41 +0200

    [UDEMY-REST-8] A JSON Response

--- src/updates/views.py
+++ src/updates/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
 
-# Create your views here.
+
+# def detail_view(request):
+#     return render() # return JSON data
+
+
+def update_model_detail_view(request):
+    data = {
+        "count": 1000,
+        "content": "Some new content"
+    }
+    return JsonResponse(data)
