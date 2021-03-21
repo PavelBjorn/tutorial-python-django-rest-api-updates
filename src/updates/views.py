804ebbbd3e5e4108cfa0563b0401f6b600c99d5a
PavelF <pavlof@wix.com>
2021-03-21 21:41:56 +0200

    [UDEMY-10] Json cbv

--- src/updates/views.py
+++ src/updates/views.py
@@ -1,10 +1,10 @@
 import json
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
+from django.views.generic import View
+from cfeapi.mixins import JsonResponseMixin
+from .models import Update
 
-
-# def detail_view(request):
-#     return render() # return JSON data
 
 def json_example_view(request):
     # URI -- FOR REST API
@@ -13,6 +13,25 @@ def json_example_view(request):
         "count": 1000,
         "content": "Some new content"
     }
-    # return JsonResponse(data)
     json_data = json.dumps(data)
     return HttpResponse(json_data, content_type='application/json')
+
+
+class JsonCBV(View):
+
+    def get(self, request, *args, **kwargs):
+        # URI -- FOR REST API
+        data = {
+            "count": 1000,
+            "content": "Some new content"
+        }
+        return JsonResponse(data)
+
+
+class JsonCBV2(JsonResponseMixin, View):
+    def get(self, request, *args, **kwargs):
+        data = {
+            "count": 1000,
+            "content": "Some new content"
+        }
+        return self.render_to_json_response(data)
