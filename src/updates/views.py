14e155cace8724e89fc8c6ec0a25dd526afaf6c2
PavelF <pavlof@wix.com>
2021-03-21 22:28:32 +0200

    [UDEMY-11] Serialize_data (#4)
    * Implement Update list and detail view
    
    * Delete db.sqlite3
    
    Co-authored-by: Pasha <pavel.fedor.develop@gmail.com>

--- src/updates/views.py
+++ src/updates/views.py
@@ -2,6 +2,7 @@ import json
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
 from django.views.generic import View
+from django.core.serializers import serialize
 from cfeapi.mixins import JsonResponseMixin
 from .models import Update
 
@@ -35,3 +36,19 @@ class JsonCBV2(JsonResponseMixin, View):
             "content": "Some new content"
         }
         return self.render_to_json_response(data)
+
+
+class SerializedView(View):
+    def get(self, request, *args, **kwargs):
+        obj = Update.objects.filter(id=1)
+        data = serialize("json", obj, fields=('user', 'content'))
+        json_data = data
+        return HttpResponse(json_data, content_type='application/json')
+
+
+class SerializedListView(View):
+    def get(self, request, *args, **kwargs):
+        qs = Update.objects.all()
+        data = serialize("json", qs, fields=('user', 'content'))
+        json_data = data
+        return HttpResponse(json_data, content_type='application/json')
