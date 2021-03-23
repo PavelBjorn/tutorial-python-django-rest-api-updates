77bd8092ce3232abcd9dd6f13cc7c3ab8eb96fec
Pasha <pavel.fedor.develop@gmail.com>
2021-03-23 21:26:14 +0200

    Implement API module

---
+++ src/api/views.py
@@ -0,0 +1,22 @@
+from updates.models import Update as UpdateModel
+
+
+# Creating, Updating, Deleting, Retrieving (1) - Update Model
+
+class UpdateModelDetailAPI:
+    def get(self, *args, **kwargs):
+        return  # json
+
+    def post(self, *args, **kwargs):
+        return  # json
+
+    def put(self, *args, **kwargs):
+        return  # json
+
+    def delete(self, *args, **kwargs):
+        return  # json
+
+
+class UpdateModelListAPI:
+    def get(self, *args, **kwargs):
+        return  # json
