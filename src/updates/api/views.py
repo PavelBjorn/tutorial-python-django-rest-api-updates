4bfe90066a3f25d5e224f76e6daed5a85a11a678
PavelF <pavlof@wix.com>
2021-03-24 19:25:40 +0200

    [UDEMY-18] Http status code (#11)
    Co-authored-by: Pasha <pavel.fedor.develop@gmail.com>

--- src/updates/api/views.py
+++ src/updates/api/views.py
@@ -3,34 +3,38 @@ from updates.models import Update as UpdateModel
 from django.views.generic import View
 from django.http import HttpResponse
 import json
+from cfeapi.mixins import HttpResponseMixin
 
 
 # Creating, Updating, Deleting, Retrieving (1) - Update Model
 
-class UpdateModelDetailAPI(View):
+class UpdateModelDetailAPI(HttpResponseMixin, View):
+    is_json = True
+
     def get(self, request, update_id, *args, **kwargs):
         obj = UpdateModel.objects.get(id=update_id)
         json_data = obj.serialize()
-        return HttpResponse(json_data, content_type="application/json")
+        return self.render_to_response(json_data)
 
     def put(self, *args, **kwargs):
-        return HttpResponse({}, content_type="application/json")
+        return self.render_to_response({}, 403)
 
     def delete(self, *args, **kwargs):
-        return HttpResponse({}, content_type="application/json")
+        return self.render_to_response({}, 403)
 
 
-class UpdateModelListAPI(CSRFExemptMixin, View):
+class UpdateModelListAPI(HttpResponseMixin, CSRFExemptMixin, View):
+    is_json = True
 
     def get(self, *args, **kwargs):
         qs = UpdateModel.objects.all()
         json_data = qs.serialize()
-        return HttpResponse(json_data, content_type="application/json")
+        return self.render_to_response(json_data)
 
     def post(self, *args, **kwargs):
         data = json.dumps("message: Unknown Data")
-        return HttpResponse(data, content_type="application/json")
+        return self.render_to_response(data=data, status=400)
 
     def delete(self, *args, **kwargs):
         data = json.dumps("message: You  can't delete whole list")
-        return HttpResponse(data, content_type="application/json")
+        return self.render_to_response(data=data, status=403)
