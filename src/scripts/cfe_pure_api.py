ec6eb256e01bd957d85e7b4d3e63509abe202a89
Pasha <pavel.fedor.develop@gmail.com>
2021-03-25 14:15:46 +0200

    Implement get object for detail view

--- src/scripts/cfe_pure_api.py
+++ src/scripts/cfe_pure_api.py
@@ -33,5 +33,33 @@ def create_update():
     return r.text
 
 
+def do_obj_update():
+    new_data = json.dumps({
+        'id': 1,
+        "content": "Some valid data"
+    })
+    r = requests.put(BASE_URL + ENDPOINT + "1/", data=new_data)
+    print("Status Code: " + str(r.status_code))
+    print("Headers: " + str(r.headers))
+
+    if r.status_code == requests.codes.ok:
+        return r.json()
+
+    return r.text
+
+
+def do_obj_delete():
+    r = requests.delete(BASE_URL + ENDPOINT + "2/")
+    print("Status Code: " + str(r.status_code))
+    print("Headers: " + str(r.headers))
+
+    if r.status_code == requests.codes.ok:
+        return r.json()
+
+    return r.text
+
+
 # get_list()
-print(create_update())
+# print(create_update())
+# print(do_obj_update())
+print(do_obj_delete())
