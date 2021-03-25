d2528de37dee966b170ce77fe48b44b50ae73c22
Pasha <pavel.fedor.develop@gmail.com>
2021-03-25 14:43:59 +0200

    Implement json validation

--- src/scripts/cfe_pure_api.py
+++ src/scripts/cfe_pure_api.py
@@ -24,7 +24,7 @@ def create_update():
         "user": 1,
         "content": "Some valid data"
     }
-    r = requests.post(BASE_URL + ENDPOINT + "1/", data=new_data)
+    r = requests.post(BASE_URL + ENDPOINT, data=json.dumps(new_data))
     print("Status Code: " + str(r.status_code))
 
     if r.status_code == requests.codes.ok:
@@ -34,11 +34,11 @@ def create_update():
 
 
 def do_obj_update():
-    new_data = json.dumps({
+    new_data = {
         'id': 1,
         "content": "Some valid data"
-    })
-    r = requests.put(BASE_URL + ENDPOINT + "1/", data=new_data)
+    }
+    r = requests.put(BASE_URL + ENDPOINT + "1/", data=json.dumps(new_data))
     print("Status Code: " + str(r.status_code))
     print("Headers: " + str(r.headers))
 
@@ -60,6 +60,6 @@ def do_obj_delete():
 
 
 # get_list()
-# print(create_update())
+print(create_update())
 # print(do_obj_update())
-print(do_obj_delete())
+# print(do_obj_delete())
