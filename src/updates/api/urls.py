0c0acca1ca311ec1e838e4c1463b56db6fab6aef
PavelF <pavlof@wix.com>
2021-03-25 16:31:20 +0200

    [UDEMY-23] One endpoint to rule the model (#16)
    Co-authored-by: Pasha <pavel.fedor.develop@gmail.com>

--- src/updates/api/urls.py
+++ src/updates/api/urls.py
@@ -1,11 +1,9 @@
 from django.conf.urls import url
 
 from .views import (
-    UpdateModelDetailAPI,
     UpdateModelListAPI
 )
 
 urlpatterns = [
     url(r'^$', UpdateModelListAPI.as_view()),
-    url(r'(?P<update_id>\d+)/$', UpdateModelDetailAPI.as_view())
 ]
