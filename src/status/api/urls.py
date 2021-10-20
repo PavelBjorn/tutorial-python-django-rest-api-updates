3a1b34dd696cc74ee19190a52af21a694ce72441
Pasha <pavel.fedor.develop@gmail.com>
2021-10-20 15:33:07 +0300

    Create single endpoint for all CRUD operations

--- src/status/api/urls.py
+++ src/status/api/urls.py
@@ -1,9 +1,7 @@
 from django.conf.urls import url
 
 from .views import StatusAPIView
-from .views import StatusDetailAPIView
 
 urlpatterns = [
     url(r'^$', StatusAPIView.as_view()),
-    url(r'^(?P<id>\d+)/$', StatusDetailAPIView.as_view()),
 ]
--- src/status/api/views.py
+++ src/status/api/views.py
@@ -1,33 +1,50 @@
 import rest_framework.mixins as mixins
 import rest_framework.generics as generics
+from django.shortcuts import get_object_or_404
 
 from status.models import Status
 from .serializers import StatusSerializer
 
 
-class StatusAPIView(mixins.CreateModelMixin, generics.ListAPIView):
+class StatusAPIView(
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    generics.ListAPIView
+):
     permission_classes = []
     authentication_classes = []
     serializer_class = StatusSerializer
 
     def get_queryset(self):
         qs = Status.objects.all()
-        query = self.request.GET.get('q')
+        request = self.request
+        query = request.GET.get('q')
         if query is not None:
             qs = qs.filter(content__icontains=query)
         return qs
 
+    def get_object(self):
+        request = self.request
+        id_param = request.GET.get('id', None)
+        query_set = self.get_queryset()
+        obj = None
+
+        if id_param is not None:
+            obj = get_object_or_404(query_set, id=id_param)
+            self.check_object_permissions(request, object)
+
+        return obj
+
+    def get(self, request, *args, **kwargs):
+        id_param = request.GET.get('id', None)
+        if id_param is not None:
+            return self.retrieve(request, *args, **kwargs)
+        return super().get(request, *args, **kwargs)
+
     def post(self, request, *args, **kwargs):
         return self.create(request, args, kwargs)
-
-
-class StatusDetailAPIView(mixins.DestroyModelMixin, mixins.UpdateModelMixin, generics.RetrieveAPIView):
-    permission_classes = []
-    authentication_classes = []
-    serializer_class = StatusSerializer
-
-    lookup_field = 'id'  # "pk" - is default param. Changes "pk" to "id" for automatically object retrieving
-    queryset = Status.objects.all()
 
     def put(self, request, *args, **kwargs):
         return self.update(request, args, kwargs)
