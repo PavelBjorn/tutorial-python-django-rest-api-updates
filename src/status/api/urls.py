846f870828f0bd5c7a4457fb01325548652e077d
PavloFedor <91895329+PavloFedor@users.noreply.github.com>
2021-10-22 18:52:52 +0300

    UDEMY-40] Views for CRUDL (#31)

--- src/status/api/urls.py
+++ src/status/api/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 
 from .views import StatusAPIView
+from .views import StatusAPIDetailView
 
 urlpatterns = [
     url(r'^$', StatusAPIView.as_view()),
+    url(r'^(?P<id>\d+)/$', StatusAPIDetailView.as_view()),
 ]
--- src/status/api/views.py
+++ src/status/api/views.py
@@ -17,11 +17,30 @@ def is_json(json_data):
     return is_valid
 
 
-class StatusAPIView(
-    mixins.CreateModelMixin,
-    mixins.RetrieveModelMixin,
+class StatusAPIDetailView(
     mixins.UpdateModelMixin,
     mixins.DestroyModelMixin,
+    generics.RetrieveAPIView
+):
+    permission_classes = []
+    authentication_classes = []
+    serializer_class = StatusSerializer
+    queryset = Status.objects.all()
+    lookup_field = 'id'
+
+    def put(self, request, *args, **kwargs):
+        print(request.data)
+        return self.update(request, args, kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.update(request, args, kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, args, kwargs)
+
+
+class StatusAPIView(
+    mixins.CreateModelMixin,
     generics.ListAPIView
 ):
     permission_classes = []
@@ -37,52 +56,6 @@ class StatusAPIView(
             qs = qs.filter(content__icontains=query)
         return qs
 
-    def get_object(self):
-        query_set = self.get_queryset()
-        obj = None
-
-        print(self.object_id)
-        if self.object_id is not None:
-            obj = get_object_or_404(query_set, id=self.object_id)
-            self.check_object_permissions(self.request, object)
-
-        return obj
-
-    def get(self, request, *args, **kwargs):
-        self._retrieve_id(request)
-
-        if self.object_id is not None:
-            return self.retrieve(request, *args, **kwargs)
-        return super().get(request, *args, **kwargs)
-
     def post(self, request, *args, **kwargs):
         return self.create(request, args, kwargs)
 
-    def put(self, request, *args, **kwargs):
-        self._retrieve_id(request)
-        return self.update(request, args, kwargs)
-
-    def patch(self, request, *args, **kwargs):
-        self._retrieve_id(request)
-        return self.update(request, args, kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        self._retrieve_id(request)
-        return self.destroy(request, args, kwargs)
-
-    def perform_destroy(self, instance):
-        if instance is not None:
-           return instance.delete()
-        return None
-
-    def _retrieve_id(self, request):
-        self.object_id = None
-        id_param = request.GET.get('id', None)
-        if id_param is None:
-            body = request.body
-            if is_json(body):
-                self.object_id = json.loads(body).get('id', None)
-        else:
-            self.object_id = id_param
-
-        print(request.body)
