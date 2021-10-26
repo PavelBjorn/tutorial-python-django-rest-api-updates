1de36f3b58cb1abced6c73e06079461be58927ef
Pasha <pavel.fedor.develop@gmail.com>
2021-10-26 14:49:10 +0300

    Test with scripts

--- src/scripts/cfe_rest_framework_api.py
+++ src/scripts/cfe_rest_framework_api.py
@@ -4,36 +4,51 @@ import os
 
 ENDPOINT = "http://127.0.0.1:8000/api/status/"
 
-# TODO detect hoe to send multipart data to make put method works
-def do(method='get', id=None, data={}, is_json=True, image_path=None):
-    headers = {}
-    if is_json:
-        headers['content-type'] = 'application/json'
-        data = json.dumps(data)
+get_data = ENDPOINT + str(22) + "/"
 
-    print(image_path)
+r = requests.get(get_data)
+print("R -> " + str(r.text))
 
-    url = ENDPOINT
-    if id is not None:
-        url = url + id + "/"
+r2 = requests.get(ENDPOINT)
+print("R2 -> " + str(r2.status_code))
 
-    print(url)
-    if image_path is not None:
-        with open(image_path, mode="rb") as image:
-            fileData = {
-                'image': image
-            }
-            r = requests.request(method, url, data=data, files=fileData)
-    else:
-        r = requests.request(method, url, data=data, headers=headers)
+post_data = json.dumps({"content": "Some random content"})
+post_headers = {
+    "content-type": "application/json"
+}
+post_response = requests.put(get_data, data=post_data, headers=post_headers)
+print("POST respose -> " + str(post_response.text))
 
-    print("Url: " + r.url)
-    print(r.status_code)
-    print(r.text)
-    return r
-
-
-do(method="put", id='21', data={'user': 1, "conten": "Content With Image"}, is_json=False, image_path=input("Image path: "))
+# # TODO detect hoe to send multipart data to make put method works
+# def do(method='get', id=None, data={}, is_json=True, image_path=None):
+#     headers = {}
+#     if is_json:
+#         headers['content-type'] = 'application/json'
+#         data = json.dumps(data)
+#
+#     print(image_path)
+#
+#     url = ENDPOINT
+#     if id is not None:
+#         url = url + id + "/"
+#
+#     print(url)
+#     if image_path is not None:
+#         with open(image_path, mode="rb") as image:
+#             fileData = {
+#                 'image': image
+#             }
+#             r = requests.request(method, url, data=data, files=fileData)
+#     else:
+#         r = requests.request(method, url, data=data, headers=headers)
+#
+#     print("Url: " + r.url)
+#     print(r.status_code)
+#     print(r.text)
+#     return r
+#
+#
+# do(method="put", id='21', data={'user': 1, "conten": "Content With Image"}, is_json=False, image_path=input("Image path: "))
 
 # do(method="put", id='1', data={'user': 1, "content": "try to update content"}, is_json=True)
 
