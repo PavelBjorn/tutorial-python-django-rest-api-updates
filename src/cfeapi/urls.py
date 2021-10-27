d031838298a9f4f495132c216160e72b8f9eaa57
PavloFedor <91895329+PavloFedor@users.noreply.github.com>
2021-10-27 13:41:54 +0300

    [UDEMY-47] Custom atuhentication view (#38)

--- src/cfeapi/urls.py
+++ src/cfeapi/urls.py
@@ -16,12 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
-from rest_framework_jwt.views import refresh_jwt_token, obtain_jwt_token
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'api/auth/jwt/$', obtain_jwt_token),
-    url(r'api/auth/jwt/refresh/$', refresh_jwt_token),
     url(r'^api/updates/', include('updates.api.urls')),  # api/updates/1/ --> detail
+    url(r'^api/auth/', include('accounts.api.urls')),  # api/updates/1/ --> detail
     url(r'^api/status/', include('status.api.urls'))
 ]
