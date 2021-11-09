8c7a1ba3652ad3f4e9315cd47a73898f23ca888e
Pasha <pavel.fedor.develop@gmail.com>
2021-11-09 16:12:29 +0200

    [UDEMY-56] Nested Serializer Part-3

--- src/accounts/api/user/serializers.py
+++ src/accounts/api/user/serializers.py
@@ -1,12 +1,13 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from status.api.serializers import StatusInlineUserSerializer
 
 User = get_user_model()
 
 
 class UserDetailSerializer(serializers.ModelSerializer):
     url = serializers.SerializerMethodField(read_only=True)
-    status_list = serializers.SerializerMethodField(read_only=True)
+    status = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = User
@@ -14,11 +15,24 @@ class UserDetailSerializer(serializers.ModelSerializer):
             'id',
             'username',
             'url',
-            'status_list'
+            'status',
         ]
 
     def get_url(self, obj):
-        return "/api/users/{name}/".format(name=obj.username)
+        return "/api/user/{name}/".format(name=obj.username)
 
-    def get_status_list(self, obj):
-        return "Not implemented yet" #TODO
+    def get_status(self, obj):
+        request = self.context.get('request')
+        limit = 10
+        if request:
+            limit_args = request.GET.get('limit')
+            try:
+                limit = int(limit_args)
+            except:
+                pass
+        qs = obj.status_set.all().order_by("-timestamp")
+        return {
+            'url': self.get_url(obj) + "status/",
+            'last': StatusInlineUserSerializer(qs.first()).data,
+            'recent': StatusInlineUserSerializer(qs[:limit], many=True).data
+        }
