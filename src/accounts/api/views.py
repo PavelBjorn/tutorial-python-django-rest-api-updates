a4adaa0f653e12c20951b60977e2e52189f81fef
PavloFedor <91895329+PavloFedor@users.noreply.github.com>
2021-11-09 11:44:18 +0200

    [UDEMY-51] Get Context Data (#42)

--- src/accounts/api/views.py
+++ src/accounts/api/views.py
@@ -40,40 +40,14 @@ class AuthAPIView(APIView):
                 return Response(respones)
         return Response({'detail': 'Invalida credentials'}, status=401)
 
+
 class RegisterAPIView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserRegisterSerailizer
     permission_classes = [permissions.AllowAny]
 
-# class RegisterAPIView(APIView):
-#     permission_classes = [permissions.AllowAny]
-#
-#     def post(self, request, *args, **kwargs):
-#         if request.user.is_authenticated:
-#             return Response({'detail': "Already registred and are authenticated!"}, status=400)
-#
-#         data = request.data
-#         username = data.get('username')
-#         email = data.get('email')
-#         password = data.get('password')
-#         password2 = data.get('password2')
-#
-#         qs = User.objects.filter(
-#             Q(username__iexact=username) |
-#             Q(email__iexact=username)
-#         )
-#
-#         if password != password2:
-#             return Response({"password": "Password must match."}, status=401)
-#         if qs.exists():
-#             return Response({"detail": "This user already exists"}, status=401)
-#         else:
-#             user = User.objects.create(username=username, email=email)
-#             user.set_password(raw_password=password)
-#             user.save()
-#             payload = jwt_payload_handler(user)
-#             token = jwt_encode_handler(payload)
-#             respones = jwt_respose_payload_handler(token, user, request=request)
-#             return Response(respones, status=201)
-#
-#         return Response({'detail': 'Invalida Request'}, status=401)
+    # set data to serializer's context
+    # to get it in serializer we should use self.contect['request']
+    def get_serializer_context(self, *args, **kwargs):
+        return {"request": self.request}
+
