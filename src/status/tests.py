8978b6aef307b28d85d1f1016afacaaca0cd5754
PavloFedor <91895329+PavloFedor@users.noreply.github.com>
2021-11-10 16:24:16 +0200

    [UDEMY-61] Automated testing (#52)

--- src/status/tests.py
+++ src/status/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.contrib.auth import get_user_model
+from .models import Status
+
+User = get_user_model()
+
+
+class StatusTestCase(TestCase):
+    username = 'pashaTest'
+
+    def setUp(self):
+        user = User.objects.create(username=self.username, email="pasha@test.com")
+        user.set_password("testPAsword")
+        user.save()
+
+    def test_create_status(self):
+        user = User.objects.get(username=self.username)
+        obj = Status.objects.create(user=user, content='Test content')
+        self.assertEqual(obj.id, 1)
+        qs = Status.objects.all()
+        self.assertEqual(qs.count(), 1)
