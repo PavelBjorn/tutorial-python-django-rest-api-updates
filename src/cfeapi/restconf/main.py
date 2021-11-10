d1139bc6f597b15ff05cadc5f4167c814f9dfbc0
PavloFedor <91895329+PavloFedor@users.noreply.github.com>
2021-11-10 11:13:53 +0200

    [UDEMY-57] Pagination to Manage request load (#48)

--- src/cfeapi/restconf/main.py
+++ src/cfeapi/restconf/main.py
@@ -7,7 +7,8 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'cfeapi.restconf.pagination.CustomPagination'
 }
 
 JWT_AUTH = {
